5cc70b8063050e5e41ea997d635fc327296ebf8d
Adham Salama <adham.salama@wuilt.com>
2023-04-29 04:32:59 +0300

    Refactor dashboard

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -1,9 +1,37 @@
-import { Route, Routes } from "react-router";
 import { HTTPMethod, io } from "../api";
 import LineChart from "../components/LineChart";
 import BarChart from "../components/PieChart";
-import Login from "../components/SignIn";
+import { config } from "../../config";
 function Dashboard() {
+  function getMonthData(year: number, month: number) {
+    return io(
+      config.baseUrl + `/expenses/statistics/${year}/${month}`,
+      HTTPMethod.GET,
+      null,
+      {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Content-Type": "application/json",
+      }
+    );
+  }
+  function getYearData(year: number) {
+    const res = io(
+      `http://localhost:4000/expenses/statistics/yearly/${year}`,
+      HTTPMethod.GET,
+      null,
+      { Authorization: `Bearer ${localStorage.getItem("token")}` }
+    );
+    return res;
+  }
+  const { data: currentYearCurrentMonth } = getMonthData(
+    new Date().getFullYear(),
+    new Date().getMonth() + 1
+  ) as { data: { _id: number; sum: number }[] };
+  const { data: previousYearCurrentMonth } = getMonthData(
+    new Date().getFullYear() - 1,
+    new Date().getMonth() + 1
+  ) as { data: { _id: number; sum: number }[] };
+
   const { data } = io(
     "http://localhost:4000/expenses/statistics",
     HTTPMethod.GET,
@@ -27,112 +55,63 @@ function Dashboard() {
     "November",
     "December",
   ];
-  console.log({ dataaa: data });
-
-  function getMonthsData(year: number) {
-    const res = io(
-      `http://localhost:4000/expenses/statistics/yearly/${year}`,
-      HTTPMethod.GET,
-      null,
-      { Authorization: `Bearer ${localStorage.getItem("token")}` }
-    );
-    return res;
+  const yearsData: { label: string; data: number[] }[] = [];
+  for (let i = 0; i < 3; i++) {
+    const { data: yearData } = getYearData(new Date().getFullYear() - i) as {
+      data?: { _id: number; sum: number }[];
+    };
+    yearsData.push({
+      label: (new Date().getFullYear() - i).toString(),
+      data: yearData?.map((y) => y.sum) ?? [],
+    });
   }
-  const { data: janData } = io(
-    `http://localhost:4000/expenses/statistics/${2023}/${1}`,
-    HTTPMethod.GET,
-    null,
-    { Authorization: `Bearer ${localStorage.getItem("token")}` }
-  ) as { data?: { _id: number; sum: number }[] };
-  const { data: febData } = io(
-    `http://localhost:4000/expenses/statistics/${2023}/${2}`,
-    HTTPMethod.GET,
-    null,
-    { Authorization: `Bearer ${localStorage.getItem("token")}` }
-  ) as { data?: { _id: number; sum: number }[] };
-
-  const { data: marchData } = io(
-    `http://localhost:4000/expenses/statistics/${2023}/${3}`,
-    HTTPMethod.GET,
-    null,
-    { Authorization: `Bearer ${localStorage.getItem("token")}` }
-  ) as { data?: { _id: number; sum: number }[] };
-
-  const { data: data2023 } = getMonthsData(2023) as {
-    data: { _id: number; sum: number }[];
-  };
-  const { data: data2022 } = getMonthsData(2022) as {
-    data: { _id: number; sum: number }[];
-  };
-  const { data: data2021 } = getMonthsData(2021) as {
-    data: { _id: number; sum: number }[];
-  };
-  const { data: data2020 } = getMonthsData(2020) as {
-    data: { _id: number; sum: number }[];
-  };
+  const monthsData: { label: string; data: number[] }[] = [];
+  for (
+    let i = 0,
+      year = new Date().getFullYear(),
+      month = new Date().getMonth() + 1 - i;
+    i < 2;
+    i++ && month > 0
+  ) {
+    const { data: monthData } = getMonthData(year, month) as {
+      data?: { _id: number; sum: number }[];
+    };
+    monthsData.push({
+      label: months[month],
+      data: monthData?.map((y) => y.sum) ?? [],
+    });
+  }
   if (!localStorage.getItem("token")) {
     return <h1>Not logged in</h1>;
   }
   return (
-    <>
-      <div
-        style={{
-          width: "50%",
-          height: "50%",
-          alignContent: "center",
-          alignItems: "center",
-          margin: "auto",
-        }}
-      >
-        <BarChart
-          label="Expenses by tags"
-          labels={data?.map((item) => item._id) ?? []}
-          data={data?.map((item) => item.totalPrice) ?? []}
-        />
-        <LineChart
-          label="Expenses per month"
-          labels={months}
-          datasets={[
-            {
-              label: "2023",
-              data: data2023?.map((i) => i.sum),
-            },
-            {
-              label: "2022",
-              data: data2022?.map((i) => i.sum),
-            },
-            {
-              label: "2021",
-              data: data2021?.map((i) => i.sum),
-            },
-            {
-              label: "2020",
-              data: data2020?.map((i) => i.sum),
-            },
-          ]}
-        />
-        <LineChart
-          label="Expenses per day"
-          labels={((n: any) => {
-            return [...Array(n).keys()];
-          })(31)}
-          datasets={[
-            {
-              label: "January",
-              data: janData?.map((i) => i.sum) ?? [],
-            },
-            {
-              label: "February",
-              data: febData?.map((i) => i.sum) ?? [],
-            },
-            {
-              label: "March",
-              data: marchData?.map((i) => i.sum) ?? [],
-            },
-          ]}
-        />
-      </div>
-    </>
+    <div
+      style={{
+        width: "50%",
+        height: "50%",
+        alignContent: "center",
+        alignItems: "center",
+        margin: "auto",
+      }}
+    >
+      <BarChart
+        label="Expenses by tags"
+        labels={data?.map((item) => item._id) ?? []}
+        data={data?.map((item) => item.totalPrice) ?? []}
+      />
+      <LineChart
+        label={`Expenses per month for last ${yearsData.length} years`}
+        labels={months}
+        datasets={yearsData}
+      />
+      <LineChart
+        label={`Expenses per day for last ${monthsData.length} months`}
+        labels={((n: any) => {
+          return [...Array(n).keys()];
+        })(31)}
+        datasets={monthsData}
+      />
+    </div>
   );
 }
 
