7e86c86ea996b74e3f20dfc8ae2eb7bff5f5d965
Adham Salama <adham.salama@wuilt.com>
2023-04-29 04:32:19 +0300

    Remove dropdown

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -25,19 +25,6 @@ function App() {
               <Nav.Link href="/">Home</Nav.Link>
               <Nav.Link href="/dashboard">Dashboard</Nav.Link>
               <Nav.Link href="/search">Search</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
             </Nav>
           </Navbar.Collapse>
           <Navbar.Collapse className="justify-content-end">
