391b9e2e461660970bb70b43648f66787e6d459d
Adham Salama <adham.salama@wuilt.com>
2023-05-04 19:32:45 +0300

    Use global validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as mongoose from 'mongoose';
+import { ValidationPipe } from '@nestjs/common';
 
 // somewhere in your code
 mongoose.set('debug', true);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('Checkout')
     .setDescription('The Checkout API description')
