48764b0612752ffa29d01ac843464d1ce1520ef2
Adham Salama <adham.salama@wuilt.com>
2023-05-04 19:34:17 +0300

    Validate DTOs

--- backend/src/expenses/dto/create-expense.dto.ts
+++ backend/src/expenses/dto/create-expense.dto.ts
@@ -1,9 +1,32 @@
+import {
+  IsString,
+  IsNumber,
+  IsArray,
+  IsOptional,
+  IsDate,
+} from 'class-validator';
+
 export class CreateExpenseDto {
+  @IsString()
   name: string;
+
+  @IsNumber()
   price: number;
+
+  @IsNumber()
   quantity: number;
+
+  @IsArray()
   tags: string[];
+
+  @IsOptional()
+  @IsString()
   comment?: string;
+
+  @IsDate()
   date: Date;
+
+  @IsOptional()
+  @IsString()
   sellerName?: string;
 }
--- backend/src/payments/dto/create-payment.dto.ts
+++ backend/src/payments/dto/create-payment.dto.ts
@@ -1,5 +1,14 @@
+import { IsString, IsNotEmpty, IsNumber, Min, IsDate } from 'class-validator';
+
 export class CreatePaymentDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsNumber()
+  @Min(0)
   value: number;
+
+  @IsDate()
   date: Date;
 }
